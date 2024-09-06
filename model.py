1a53096d20fe1184f957c60d64885df99f1c6258
fahad-abrar <fahad.abrarr@gmail.com>
2024-09-06 15:33:06 +0600

    review prdiction model is added

---
+++ model.py
@@ -0,0 +1,60 @@
+import re
+import pickle
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+class Review_Prediction_model:
+    '''
+    # Initialize the PorterStemmer
+    # Load the pre-fitted TfidfVectorizer
+    # Load the pre-trained model
+
+    '''
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+        self.vectorizer = pickle.load(open('ml-model/vectorizer.sav', 'rb'))
+        self.model = pickle.load(open('ml-model/model.sav', 'rb'))
+
+    '''
+    define a fuction for
+    remove stopwords and apply stemming
+
+    '''
+    def stemming(self, content):
+        if not isinstance(content, str):
+            content = ''
+        stem_content = re.sub('[^a-zA-Z]', ' ', content)
+        stem_content = stem_content.lower()
+        stem_content = stem_content.split()
+        stem_content = [self.stemmer.stem(word) for word in stem_content if word not in stopwords.words('english')]
+        stem_content = ' '.join(stem_content)
+        return stem_content
+
+    '''
+    # Transform the content using the pre-fitted vectorizer
+    # Return the single prediction
+    '''
+    def predict(self, content):
+        content_stemmed = self.stemming(content)
+        content_vectorized = self.vectorizer.transform([content_stemmed])
+        result = self.model.predict(content_vectorized)
+        return result[0]  
+
+    '''
+    print the final result
+    '''
+    def print_result(self, content):
+        result = self.predict(content)
+        if result == 2:
+            print('Positive review')
+        elif result == 1:
+            print('Neutral review')
+        else:
+            print('Negative review')
+
+
+if __name__ == "__main__":
+    review_model = Review_Prediction_model()
+    content = 'nice'
+    review_model.print_result(content)
+
